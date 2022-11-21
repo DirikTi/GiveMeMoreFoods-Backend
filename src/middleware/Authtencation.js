edac57c1c868f4489bb383f672202ac3f9fcfe1f
DirikTi <ertugruldirik35@gmail.com>
2022-11-21 17:05:24 +0300

    delete not useful codes in Auth

--- src/middleware/Authtencation.js
+++ src/middleware/Authtencation.js
@@ -30,53 +30,14 @@ export default function AuthMiddleware(roleCode = "") {
         let secretMessage = MyJWT.decodeToken(token);
 
         let user = await mysqlAsi.executeQueryAsync(
-            "SELECT userId, companyId, roleCodes, userFullname, userSurname, userEmail, userPhoneNumber FROM users WHERE userLoginToken=CONVERT_UUID('" + secretMessage + "') LIMIT 1"
+            "SELECT userId FROM users WHERE userLoginToken=CONVERT_UUID('" + secretMessage + "') LIMIT 1"
         );
-        
+
         if(user[0].length == 0) {
             resp.json(errorResponse(null, "JWT Token not found user ==> IP_ADRESS " + req.ip, 401));
             return -1;
         }
         
         req.userID = user[0].userId;
-        req.companyId = user[0].companyId;
-        req.roleCodes = user[0].roleCodes;
-        req.userFullname = user[0].userFullname;
-        req.userSurname = user[0].userSurname;
-        req.userEmail = user[0].userEmail;
-        req.userPhoneNumber = user[0].userPhoneNumber;
-
-        if(roleCode == "" || checkRoleName(req.roleCodes, roleCode)) {
-            next();
-        } else {
-            if(req.baseUrl == "/api/v1/order") {
-                let result = await mysqlAsi.executeQueryAsync("SELECT franchiseId FROM franchise WHERE userId='" + req.userID + " LIMIT 1");
-                if(result[0].franchiseId == req.query.id) {
-                    next();
-                } else {
-                    resp.json(errorResponse(null, "NOT_AUTH ", 401));
-                    resp.end();
-                }
-            } else {
-                resp.json(errorResponse(null, "NOT_AUTH ", 401));
-                resp.end();
-            }
-
-        }
     } 
-}
-
-function checkRoleName(roleCodes, checkingRoleCodes) {
-    let _roles = roleCodes.split(",");
-    let _checkingRoleCodes = checkingRoleCodes.split(","); 
-    for(let i = 0; i < _roles.length; i++) {
-        for(let j = 0; j < _checkingRoleCodes.length; j++) {
-            if(_checkingRoleCodes[j] == _roles[i] || _roles[i] == "admin") {
-                return true;
-            }
-        }
-        
-    }
-
-    return false;
 }
